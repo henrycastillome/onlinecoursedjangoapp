cf29ae4d2b949a5d4ddc4f961584b0723b70406a
Henry Castillo <henrycastillome@gmail.com>
2023-03-08 03:04:56 -0500

    changes

--- onlinecourse/settings.py
+++ onlinecourse/settings.py
@@ -167,5 +167,3 @@ if "CI" in os.environ:
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
-
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
